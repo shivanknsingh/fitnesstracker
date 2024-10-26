2a740475ac8421129831da148b4bcec8b0ac32a5
shivanksingh <thakurshivank2580@gmail.com>
2024-10-27 01:57:53 +0530

    Initial commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <p>
+            Elite Edge Fitness
+        </p>
+    </header>
+  )
+}
+
+export default Navbar
